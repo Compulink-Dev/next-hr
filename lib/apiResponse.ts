6d24fbeb8091749bdd9a8a4c77d04864f7ae2a55
Compulink-Dev <devcompulink@gmail.com>
2024-07-18 09:23:32 +0200

    main

--- lib/apiResponse.ts
+++ lib/apiResponse.ts
@@ -1,9 +1,7 @@
-
+export const dynamic = "force-dynamic"
 export async function getData(endpoint: any) {
     try {
-        const response = await fetch(`${process.env.URL}/api/${endpoint}`, {
-            cache: 'no-store'
-        })
+        const response = await fetch(`${process.env.URL}/api/${endpoint}`)
 
         const data = await response.json()
 
