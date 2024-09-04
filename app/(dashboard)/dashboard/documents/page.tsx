d000303e5eb0c25c664e58ee9c4f45d6930c9ec0
Compulink-Dev <devcompulink@gmail.com>
2024-09-04 15:51:21 +0200

    main

---
+++ app/(dashboard)/dashboard/documents/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Documents() {
+    return (
+        <div className='p-4'>Documents</div>
+    )
+}
+
+export default Documents
---
+++ app/(dashboard)/dashboard/integrations/page.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Form from './_components/Form'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { Separator } from '@/components/ui/separator'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+function Integrations() {
+    return (
+        <div className=''>
+            <div className="p-2">
+                <div className="flex items-center justify-between">
+                    <p className="">Integrations</p>
+                    <div className="flex items-center gap-2">
+                        <Button
+                            variant={'ghost'}
+                        >Invite user</Button>
+                        <Button
+                            variant={'outline'}
+                            className='flex items-center gap-1'>
+                            <Plus />
+                            <span className="">Create</span>
+                        </Button>
+                    </div>
+                </div>
+                <Separator className='w-full my-2.5' />
+                <div className="">
+                    <Tabs defaultValue="account" className="">
+                        <TabsList className="grid w-full grid-cols-6">
+                            <TabsTrigger value="all">All</TabsTrigger>
+                            <TabsTrigger value="most">Most used</TabsTrigger>
+                            <TabsTrigger value="sales">Sales</TabsTrigger>
+                            <TabsTrigger value="fleet">Fleet</TabsTrigger>
+                            <TabsTrigger value="calender">Calenders</TabsTrigger>
+                            <TabsTrigger value="email">Email messaging</TabsTrigger>
+                        </TabsList>
+                        <TabsContent value="all">
+                            <p className="">All integrations</p>
+                        </TabsContent>
+                        <TabsContent value="most">
+                            <p className="">Most Used</p>
+                        </TabsContent>
+                        <TabsContent value="sales">
+                            <p className="">Sales and Purchases</p>
+                        </TabsContent>
+                        <TabsContent value="fleet">
+                            <p className="">Fleet</p>
+                        </TabsContent>
+                        <TabsContent value="calender">
+                            <p className="">Calenders</p>
+                        </TabsContent>
+                        <TabsContent value="email">
+                            <p className="">Email messaging</p>
+                        </TabsContent>
+                    </Tabs>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Integrations
